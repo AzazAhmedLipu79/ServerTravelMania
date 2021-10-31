711af8e36a554edee18017f5f646ab0816bdbd32
azaz ahmed <ahsanlisan2@.com>
2021-10-31 18:26:46 +0600

    push

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
@@ -15,6 +15,10 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+});
+
+app.get("/", (req, res) => {
+  res.send("server running");
 });
 
 async function run() {
@@ -46,12 +50,6 @@ async function run() {
     //  await close
   }
 }
-
-run().catch(console.dir);
-
-app.get("/", (req, res) => {
-  res.send("server okook");
-});
 
 app.listen(port, () => {
   console.log(`Running Server on`, port);
